4e1e6398881d54043c461c4fd7eccd0d714b4148
iamsomnia <idhamaziz390@gmail.com>
2023-03-31 14:49:34 +0700

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,10 +15,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->enum('user_type', ['perorangan', 'instansi'])->default('perorangan')->nullable();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('gender', ["L", "P"])->nullable();
+            $table->string('address')->nullable();
+            $table->integer('provinsi_id')->nullable();
+            $table->integer('kotakab_id')->nullable();
+            $table->integer('kecamatan_id')->nullable();
+            $table->integer('kelurahan_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -33,4 +41,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
